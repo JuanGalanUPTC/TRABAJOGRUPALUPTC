72e47268c8b6b3a0dc9d227fea2e0126fda49ffa
JuanGalanUPTC <150257066+JuanGalanUPTC@users.noreply.github.com>
2023-11-11 16:47:32 -0500

    Update TrabajoGrupal.java

--- TrabajoGrupal.java
+++ TrabajoGrupal.java
@@ -16,6 +16,12 @@ public class TrabajoGrupal {
                     String texto = sc.nextLine();
                     String nombrePropio = convertidorNombrePropio(texto);
                     System.out.println(nombrePropio);
+                    break;
+                case 4:
+                    System.out.println("Porfavor ingresa un número");
+                    int num = sc.nextInt();
+                    egolatra(num);
+                    break;
                 default:
                     System.out.println("Opción no valida");
             }  
@@ -38,6 +44,51 @@ public class TrabajoGrupal {
             }
         }
         return resultado.toString();
+    }
+
+    public static void egolatra(int num){
+        Scanner sc = new Scanner(System.in);
+        int numero = num;
+        do {
+            System.out.print("Ingrese un número positivo: ");
+            while (!sc.hasNextInt()) {
+                System.out.println("¡Error! Por favor, ingrese un número entero positivo.");
+                sc.next(); 
+            }
+            numero = sc.nextInt();
+            if (numero <= 0) {
+                System.out.println("¡Error! Por favor, ingrese un número entero positivo.");
+            }
+        } while (numero <= 0);
+        boolean esEgolatra = esNumeroEgolatra(numero);
+        if (esEgolatra) {
+            System.out.println("Verdadero. El número es ególatra.");
+        } else {
+            System.out.println("Falso. El número no es ególatra.");
+        }
+    }
+    
+    private static boolean esNumeroEgolatra(int numero) {
+        int numDigitos = contarDigitos(numero);
+        int suma = 0;
+        int temp = numero;
+
+        while (temp > 0) {
+            int digito = temp % 10;
+            suma += Math.pow(digito, numDigitos);
+            temp /= 10;
+        }
+
+        return suma == numero;
+    }
+
+    private static int contarDigitos(int numero) {
+        int count = 0;
+        while (numero != 0) {
+            numero /= 10;
+            count++;
+        }
+        return count;
     }
 
 }
