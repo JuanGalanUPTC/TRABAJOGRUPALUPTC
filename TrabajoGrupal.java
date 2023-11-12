7eaf99fee1501c6c52fe55baf89ee5e443687b65
JuanGalanUPTC <150257066+JuanGalanUPTC@users.noreply.github.com>
2023-11-12 10:52:57 -0500

    Update TrabajoGrupal.java

--- TrabajoGrupal.java
+++ TrabajoGrupal.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class TrabajoGrupal {
@@ -19,7 +21,7 @@ public class TrabajoGrupal {
                 break;
 
             case 2:
-                
+                numerosPrimos();
                 break;
 
             case 3:
@@ -119,6 +121,64 @@ public class TrabajoGrupal {
         } else {
             System.out.println("Falso. El número no es ególatra.");
         }
+    }
+
+    public static List<Integer> primeNum(int num) {
+        List<Integer> fp = new ArrayList<Integer>();
+        int div = 2;
+        
+        while (num > 1) {
+            if (num % div == 0) {
+                fp.add(div);
+                num = num / div;
+            } else {
+                div++;
+            }
+        }
+        
+        return fp;
+    }
+    
+    public static String view(List<Integer> fp1) {
+        StringBuilder res = new StringBuilder();
+        
+        int fc = fp1.get(0);
+        int exp = 1;
+        
+        for (int i = 1; i < fp1.size(); i++) {
+            if (fp1.get(i) == fc) {
+                exp++;
+            } else {
+                res.append(fc);
+                if (exp > 1) {
+                    res.append("^").append(exp);
+                }
+                res.append(" * ");
+                fc = fp1.get(i);
+                exp = 1;
+            }
+        }
+        
+        res.append(fc);
+        if (exp > 1) {
+            res.append("^").append(exp);
+        }
+        
+        return res.toString();
+    }
+     
+    public static void numerosPrimos() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese un número entero positivo: ");
+        int numero = sc.nextInt();
+        
+        List<Integer> fp2 = primeNum(numero);
+        String resultado = view(fp2);
+        
+        System.out.println("Los factores primos de " + numero + " son: " + fp2);
+        System.out.println("La expresión en formato de potencias es: " + resultado);
+        
+        sc.close();
     }
 
     private static boolean esNumeroEgolatra(int numero) {
