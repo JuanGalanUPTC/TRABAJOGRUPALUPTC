45ab111313d95542e05fa04e9ae2f622b3700cad
JuanGalanUPTC <150257066+JuanGalanUPTC@users.noreply.github.com>
2023-11-12 09:39:12 -0500

    Update TrabajoGrupal.java

--- TrabajoGrupal.java
+++ TrabajoGrupal.java
@@ -15,7 +15,7 @@ public class TrabajoGrupal {
 
         switch (op) {
             case 1:
-                
+                numerosRomanos();
                 break;
 
             case 2:
@@ -40,6 +40,40 @@ public class TrabajoGrupal {
             default:
                 System.out.println("Opción no válida");
         }
+    }
+
+    private static final int [] num={ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String [] rom={"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    
+    public static String ConvertNumToRom(int num1){
+        if (num1<1 || num1>3000){
+          
+            throw new IllegalArgumentException("El número debe estar entre 1 y 3000.");
+        }
+
+        StringBuilder res= new StringBuilder();
+        for (int i=0; i<num.length;i++){
+            while (num1>=num [i]){
+                res.append(rom[i]);
+                num1-=num[i];
+            }
+        }
+        return res.toString();
+    }
+
+    public static void numerosRomanos(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Ingrese un número entre 1 y 3000");
+        int num1= sc.nextInt();
+        try {
+            String numRom = ConvertNumToRom(num1);
+            System.out.println("El número "+" en números romanos es: "+ numRom);
+
+        } catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+
+        }
+        sc.close(); 
     }
 
     public static String convertidorNombrePropio(String texto) {
